7b360adf088806bbd893e2e0cf21280ef8858a73
Dima <karyakin.dmitry@gmail.com>
2026-08-19 02:14:44 +0200

    lint

--- pages/base.ts
+++ pages/base.ts
@@ -1,19 +1,19 @@
-import {Page} from '@playwright/test';
+import { Page } from '@playwright/test'
 
 export abstract class BasePage {
-  private readonly _page: Page;
-  private readonly url: string;
+    private readonly _page: Page
+    private readonly url: string
 
-  constructor(page: Page, url: string) {
-    this._page = page;
-    this.url = url;
-  }
+    constructor(page: Page, url: string) {
+        this._page = page
+        this.url = url
+    }
 
-  get page(): Page {
-    return this._page;
-  }
+    get page(): Page {
+        return this._page
+    }
 
-  async open() {
-    await this.page.goto(this.url);
-  }
+    async open() {
+        await this.page.goto(this.url)
+    }
 }
--- pages/browser.ts
+++ pages/browser.ts
@@ -1,29 +1,33 @@
-import { Locator, Page } from "@playwright/test";
-import { BasePage } from "./base";
+import { Locator, Page } from '@playwright/test'
+import { BasePage } from './base'
 
 export class BrowserPage extends BasePage {
-  private readonly requestInputField: Locator;
-  private searchResults: Locator[] = new Array<Locator>();
+    private readonly requestInputField: Locator
+    private searchResults: Locator[] = new Array<Locator>()
 
-  constructor(page: Page) {
-    // Could've used configuration instead of hard-coding, but this page object works with a specific locators,
-    // that defeats the whole idea of parametrization at this point.
-    super(page, "https://duckduckgo.com/")
+    constructor(page: Page) {
+        // Could've used configuration instead of hard-coding, but this page object works with a specific locators,
+        // that defeats the whole idea of parametrization at this point.
+        super(page, 'https://duckduckgo.com/')
 
-    this.requestInputField = this.page.locator("//input[@id='search_form_input']")
-  }
+        this.requestInputField = this.page.locator(
+            "//input[@id='search_form_input']"
+        )
+    }
 
-  public async searchFor(query: string) {
-    await this.requestInputField.fill(query)
+    public async searchFor(query: string) {
+        await this.requestInputField.fill(query)
 
-    this.searchResults = await this.page.locator("//article[@data-testid='result']/div[2]//div/a").all();
-  }
+        this.searchResults = await this.page
+            .locator("//article[@data-testid='result']/div[2]//div/a")
+            .all()
+    }
 
-  public async navigateToFirstResult() {
-    await this.searchResults[0].click()
-  }
+    public async navigateToFirstResult() {
+        await this.searchResults[0].click()
+    }
 
-  public async shouldHaveSearchResults() {
-    // TODO Implement
-  }
+    public async shouldHaveSearchResults() {
+        // TODO Implement
+    }
 }
